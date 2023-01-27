295b03d35701329665db097c7cac5c8246cf0828
Julie Nielsen <jpettesen98@gmail.com>
2023-01-27 14:52:39 +0100

    last commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-demo/'
-})
+  base: "/vite-demo/", //fjern hvis man skal hoste utenfor github pages/via actions
+});
